ae865d3ad2db7aba7c02fb9577eb1e583769d278
Duda Duarte <dudammduarte@yahoo.com.br>
2019-05-07 16:02:43 -0300

    mudanças no eslint, refatoração e novo arquivo readme

---
+++ README.md
@@ -0,0 +1,30 @@
+# Validador de Cartão de Crédito v.1.0.0
+
+**Esta biblioteca destina-se à verificação de números de cartões de crédito, retornando true para um cartão válido, e false para um inválido.**
+Na versão atual a biblioteca verifica números de cartões de crédito tanto em números quanto em string. O usuário também pode inserir o número do cartão com espaços a cada 4 dígitos, pois a verificação acontece normalmente.
+
+## Como instalar:
+
+```shell
+
+$  npm install lib-credit-card
+
+```
+
+## Como utilizar:
+
+### Validando um cartão de crédito:
+
+```node
+
+> const validate = require("../lib/lib-credit-card");
+> validate(5156701090127154)
+> // returns true
+
+```
+
+## roadmap oficial do projeto
+
+#### versão 1.0.0 (released)
+- funcionalidades: verificação da validade de cartões de crédito através de seus números.
+- aceitando input de números e strings.
--- test/test.spec.js
+++ test/test.spec.js
@@ -1,80 +1,43 @@
 let chai = require('chai');
 let expect = chai.expect;
-let cardValidator = require('../lib/libCreditCard.js');
+let cardValidator = require('../lib/lib-credit-card.js');
 
 describe('Biblioteca Cartão de Crédito', () => {
-
-  describe('função cardValidator', () => {
-
-    describe('cartões válidos', () => {
-
-      it('meu cartão virtual', () => {
-
-        expect(cardValidator(5502099757664036)).to.equal(true);
-
-      });
-      it('cartão válido aleatório', () => {
-
-        expect(cardValidator('4012 0010 3714 1112')).to.equal(true);
-
-      });
-      it('cartão válido aleatório', () => {
-
-        expect(cardValidator('5442054108091705')).to.equal(true);
-
-      });
-      it('cartão válido aleatório', () => {
-
-        expect(cardValidator(30293810466644)).to.equal(true);
-
-      });
-      it('cartão válido aleatório', () => {
-
-        expect(cardValidator('6011137424214298')).to.equal(true);
-
-      });
-      it('cartão válido aleatório', () => {
-
-        expect(cardValidator(372181965977158)).to.equal(true);
-
-      });
-
+  describe('cartões válidos', () => {
+    it('cartão válido string + espaços', () => {
+      expect(cardValidator('4012 0010 3714 1112')).to.equal(true);
     });
-    describe('cartões INválidos', () => {
-
-      it('cartão INválido', () => {
-
-        expect(cardValidator(123456789012345)).to.equal(false);
-
-      });
-      it('cartão INválido', () => {
-
-        expect(cardValidator('2003004005060708')).to.equal(false);
-
-      });
-      it('cartão INválido', () => {
-
-        expect(cardValidator('472837284059123')).to.equal(false);
-
-      });
-      it('cartão INválido', () => {
-
-        expect(cardValidator(820170273045072)).to.equal(false);
-
-      });
-      it('cartão INválido', () => {
-
-        expect(cardValidator('5502199727664036')).to.equal(false);
-
-      });
-      it('cartão INválido', () => {
-
-        expect(cardValidator(4012001037141135)).to.equal(false);
-
-      });
-
+    it('cartão válido string', () => {
+      expect(cardValidator('5442054108091705')).to.equal(true);
     });
-
+    it('cartão válido número', () => {
+      expect(cardValidator(30293810466644)).to.equal(true);
+    });
+    it('cartão válido string', () => {
+      expect(cardValidator('6011137424214298')).to.equal(true);
+    });
+    it('cartão válido número + ponto', () => {
+      expect(cardValidator(3721.81965977158)).to.equal(true);
+    });
   });
-
+  describe('cartões inválidos', () => {
+    it('cartão inválido número', () => {
+      expect(cardValidator(123456789012345)).to.equal(false);
+    });
+    it('cartão inválido string', () => {
+      expect(cardValidator('2003004005060708')).to.equal(false);
+    });
+    it('cartão inválido string', () => {
+      expect(cardValidator('472837284059123')).to.equal(false);
+    });
+    it('cartão inválido número', () => {
+      expect(cardValidator(820170273045072)).to.equal(false);
+    });
+    it('cartão inválido + espaços', () => {
+      expect(cardValidator('5502 1997 2766 4036')).to.equal(false);
+    });
+    it('cartão inválido número', () => {
+      expect(cardValidator(4012001037141135)).to.equal(false);
+    });
+  });
 });
